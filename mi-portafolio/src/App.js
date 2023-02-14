ce3042c7d860579c02bef586045a29db93fbbaba
Gustavo Ornelas <ornelasg93@gmail.com>
2023-02-14 14:42:29 -0600

    feat: Se hace Navbar con estilos.

--- mi-portafolio/src/App.js
+++ mi-portafolio/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-     
+      <NavBar />
+      
     </div>
   );
 }
